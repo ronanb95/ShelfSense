c61786dc421e74465f1cf2fbfcefef7f6cd83698
Ronan Byrne <ronan.byrne1@ucdconnect.ie>
2020-11-20 14:31:30 +0000

    Added and linked location, unit and stockControl models

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, StockControl, Location, Unit
 
 admin.site.register(Product)
+admin.site.register(StockControl)
+admin.site.register(Location)
+admin.site.register(Unit)
--- mainApp/models.py
+++ mainApp/models.py
@@ -2,6 +2,23 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import MinValueValidator
 
+#The actual weigth sensing unit and ssh details
+class Unit(models.Model):
+	unitID = models.PositiveIntegerField(auto_created=True, primary_key=True)
+	ssh = models.CharField(max_length=1000)
+
+	def __str__ (self):
+		return("Unit " + str(self.unitID))
+
+#The location within the store eg(1A is the first shelf on the first shelving unit, 1B is second shelf on first shelving unit etc..)
+class Location(models.Model):
+	locationID = models.CharField(max_length=100, primary_key=True)
+	unit = models.OneToOneField(Unit, on_delete=models.SET_NULL, null=True)
+
+	def __str__(self):
+		return("Location " + self.locationID)
+
+#The product details
 class Product(models.Model):
 	barcode = models.CharField(max_length=50, primary_key=True)
 	brand = models.CharField(max_length=100)
@@ -13,20 +30,18 @@ class Product(models.Model):
 	def __str__ (self):
 		return (self.brand + " " + self.productName )
 
+#The table that does the actual monitoring, represents a single unit actually in use
 class StockControl(models.Model):
 	stockControlId = models.IntegerField(auto_created=True, primary_key=True)
-	#barcode (FK)
-	#location (FK)
-	#quantity = Need to calculate this?? Do similar thing as the weight thing above
-	#DateTime
-	pass
+	#If product is deleted want to stop monitoring
+	barcode = models.OneToOneField(Product, on_delete=models.CASCADE)
+	#Want to stop monitoring if location is deleted
+	location = models.ForeignKey(Location, on_delete=models.CASCADE)
+	#PositiveIntegerField allows for 0 aswell 
+	quantity = models.PositiveIntegerField(default=0)
+	timeAdded = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return (self.barcode.brand + " " + self.barcode.productName + " at location " + str(self.location.locationID))
 
-class Location(models.Model):
-	locationID = models.CharField(max_length=100, primary_key=True)
-	#unit (FK)
-
-
-class Unit(models.Model):
-	UnitID = models.PositiveIntegerField(primary_key=True)
-	SSH = models.CharField(max_length=1000)
+	
