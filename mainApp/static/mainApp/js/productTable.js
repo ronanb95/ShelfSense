5680c82b5368664fcde7087e2402836fe65026e6
Yi <yi.wu1@ucdconnect.ie>
2020-12-01 15:11:06 +0000

    yuqian: removed duplicated rows in the Default Table

--- mainApp/static/mainApp/js/productTable.js
+++ mainApp/static/mainApp/js/productTable.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
+(document).ready(function () {
+    var index = [];
     var t = $('#dataTable2').DataTable();
-    var counter = 1;
-    fetch("http://localhost:8000/default")
-	.then(response => {
-	  return response.json();
-	})
-	.then(data => {
-	  data.forEach(function(code){
-	    t.row.add( [
-	    	code['barcode'],
-	    	code['productName'],
-	    	code['brand'],
-	    	code['stockcontrol__location'],
-	    	code['stockcontrol__quantity']
-	    ] ).draw( false );
-	  })
-	})
-} );
+    fetch("http://127.0.0.1:8000/default").then(response => {
+        return response.json();
+    }).then(data => {
+        data.forEach(function (code) {
+            if (!~index.indexOf(code['barcode'])) {
+                t.row.add([code['brand'], code['productName'], code['stockcontrol__location'], code['barcode'], code['stockcontrol__quantity']]).draw(false);
+            }
+            index.push(code['barcode']);
+        })
+    })
+});
 
 
